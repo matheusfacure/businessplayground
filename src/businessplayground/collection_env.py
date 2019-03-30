ef5593a81be5ef77179ec554af77ba5eadc89435
matheusfacure <matheusfacure01@gmail.com>
2019-03-30 19:33:29 -0300

    polish

--- src/businessplayground/collection_env.py
+++ src/businessplayground/collection_env.py
@@ -1,19 +1,18 @@
-import pandas as pd
 import datetime
+import uuid
 from datetime import timedelta
-from toolz import curry
 
 import numpy as np
-from matplotlib import pyplot as plt
-import uuid
-
+import pandas as pd
+from toolz import curry
+from typing import Callable, Dict
 
 @curry
 def financial_educ_fn(df: pd.DataFrame, age_col: str, fin_ed_col: str):
     n = df.shape[0]
     financial_educ = (
-        (np.random.beta(10, 20, n) +
-         np.random.beta(df[age_col]/(10 * 365), 2)) * 50 # higher than 50 after ~30 year
+            (np.random.beta(10, 20, n) +
+             np.random.beta(df[age_col] / (10 * 365), 2)) * 50  # higher than 50 after ~30 year
     )
     return df.assign(**{fin_ed_col: financial_educ.astype(int)})
 
@@ -21,23 +20,23 @@ def financial_educ_fn(df: pd.DataFrame, age_col: str, fin_ed_col: str):
 @curry
 def debt_fn(df: pd.DataFrame, fin_ed_col: str, debt_col: str):
     n = df.shape[0]
-    debt = np.random.normal(5000/df[fin_ed_col], 700, n)
+    debt = np.random.normal(5000 / df[fin_ed_col], 700, n)
     debt = np.maximum(debt, 10)
     return df.assign(**{debt_col: debt})
 
 
 @curry
-def discount_fn(df: pd.DataFrame, discount_col: str, debt_col: str, max_discount: float = 0.5):
+def discount_fn(df: pd.DataFrame, discount_col: str, max_discount: float = 0.5):
     n = df.shape[0]
-    discount = np.random.binomial(1,0.5,n)
+    discount = np.random.binomial(1, 0.5, n)
     discount = discount * np.random.beta(0.9, 4, n)
     discount = np.minimum(discount, max_discount)
     return df.assign(**{discount_col: discount})
 
 
 @curry
-def update_interest_fn(df: pd.DataFrame, intr_col: str, debt_col: str, days_late_col: int, daily_intr: float=0.004):
-    intr = np.where(df[days_late_col] < 100, df[debt_col] * daily_intr, 0) # interest only untill 100 days late
+def update_interest_fn(df: pd.DataFrame, intr_col: str, debt_col: str, days_late_col: int, daily_intr: float = 0.004):
+    intr = np.where(df[days_late_col] < 100, df[debt_col] * daily_intr, 0)  # interest only untill 100 days late
     return df.assign(**{intr_col: df[intr_col] + intr})
 
 
@@ -45,18 +44,15 @@ def update_interest_fn(df: pd.DataFrame, intr_col: str, debt_col: str, days_late
 def update_fin_ed(df: pd.DataFrame, fin_ed_col: str, lr_rate: float):
     n = df.shape[0]
     financial_educ = (
-        ((df[fin_ed_col] * (1-lr_rate)) +
+        ((df[fin_ed_col] * (1 - lr_rate)) +
          (np.random.beta(10, 1, n) * 100 * lr_rate))
     )
     return df.assign(**{fin_ed_col: financial_educ})
 
 
 @curry
-def payment_fn(df: pd.DataFrame, payment_col: str, initil_debt_col: str, debt_col: str,
-               intr_col: str, fin_ed_col: str):
-    
-    n = df.shape[0]
-    payment = np.random.normal((50 + 
+def payment_fn(df: pd.DataFrame, payment_col: str, initil_debt_col: str, debt_col: str, intr_col: str, fin_ed_col: str):
+    payment = np.random.normal((50 +
                                 .05 * df[initil_debt_col] -
                                 2.0 * df[intr_col] +
                                 10 * (df[fin_ed_col] - 50)),
@@ -67,14 +63,12 @@ def payment_fn(df: pd.DataFrame, payment_col: str, initil_debt_col: str, debt_co
 
 @curry
 def update_debt(df: pd.DataFrame, debt_col: str, payment_col: str, intr_col: str):
-    n = df.shape[0]
     debt = df[debt_col] + df[intr_col] - df[payment_col]
     return df.assign(**{debt_col: debt})
 
 
-
-class envoriment(object):
-    def __init__(self,):
+class Environment(object):
+    def __init__(self, ):
         self.date = datetime.datetime(2016, 1, 1)
         self.history = pd.DataFrame({
             "id": [str(uuid.uuid1())],
@@ -83,64 +77,61 @@ class envoriment(object):
             "debt": [1000.],
             "days_late": [1],
             "payment": [0.],
-            "age":[20 * 365],
+            "age": [20 * 365],
             "discount": [0.],
             "interest": [0.],
-            "action":[np.nan],
-            
+            "action": [np.nan],
+
             # hidden variables
             "financial_educ": [45.],
         })
-        
-    def get_new_customers(self, date, n):
+
+    @staticmethod
+    def get_new_customers(date: datetime.datetime, n: int):
         return (pd.DataFrame({
             "id": [str(uuid.uuid1()) for _ in range(n)],
             "age": ((np.random.gamma(2, 10, n) + 18) * 365).astype(int),
         })
-                .assign(date = date,
-                        days_late = 1,
-                        payment = 0.,
-                        discount = 0.,
-                        interest = 0.0,
-                        action = np.nan)
+                .assign(date=date,
+                        days_late=1,
+                        payment=0.,
+                        discount=0.,
+                        interest=0.0,
+                        action=np.nan)
                 .pipe(financial_educ_fn(age_col="age", fin_ed_col="financial_educ"))
                 .pipe(debt_fn(fin_ed_col="financial_educ", debt_col="initial_debt"))
-                .assign(debt=lambda d: d["initial_debt"])
-               )
-    
-    def update(self, old: pd.DataFrame, date):
+                .assign(debt=lambda d: d["initial_debt"]))
+
+    @staticmethod
+    def update(old: pd.DataFrame, date: datetime.datetime):
         return (old
-                .assign(date = date)
-                .assign(age = lambda d: d["age"] + 1)
-                .assign(days_late = lambda d: d["days_late"] + 1)
+                .assign(date=date)
+                .assign(age=lambda d: d["age"] + 1)
+                .assign(days_late=lambda d: d["days_late"] + 1)
                 .pipe(update_interest_fn(intr_col="interest", debt_col="debt", days_late_col="days_late"))
                 .pipe(update_fin_ed(fin_ed_col="financial_educ", lr_rate=0.01))
-                .pipe(discount_fn(discount_col="discount", debt_col="debt"))
+                .pipe(discount_fn(discount_col="discount"))
                 .pipe(payment_fn(payment_col="payment",
                                  intr_col="interest",
                                  initil_debt_col="initial_debt",
-                                 debt_col = "debt",
+                                 debt_col="debt",
                                  fin_ed_col="financial_educ"))
-                .pipe(update_debt(payment_col="payment", intr_col="interest", debt_col="debt",))
-               )
-        
-    
-    def next_day(self,):
-        
+                .pipe(update_debt(payment_col="payment", intr_col="interest", debt_col="debt", )))
+
+    def next_day(self, intervention: Dict[str, Callable[[pd.DataFrame], pd.Series]]):
         today = self.date + timedelta(days=1)
-        
+
         # new customers
         n_new_cust = np.random.poisson(5)
         new_customers = self.get_new_customers(today, n_new_cust)
-        
+
         # update old customers
         old_cust_yesterday = (self
                               .history[self.history["date"] == self.date]
-                              .query("debt>=payment")
-                             )
-        
-        old_cust_today = self.update(old_cust_yesterday, today)
-        
+                              .query("debt>=payment"))
+
+        old_cust_today = (self.update(old_cust_yesterday, today)
+                          .assign(**intervention))
+
         self.date = today
         self.history = pd.concat([self.history, old_cust_today, new_customers], sort=False).reset_index(drop=True)
-    
